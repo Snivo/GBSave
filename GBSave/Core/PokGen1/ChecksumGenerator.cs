e2172ffecdd285af23e1b46642859a012a4d3de1
Snivo <jrgerba@gmail.com>
2022-04-10 16:49:57 -0400

    Added gen 1 checksum algorithm

---
+++ GBSave/Core/PokGen1/ChecksumGenerator.cs
@@ -0,0 +1,23 @@
+﻿namespace GBSave.Core.PokGen1;
+
+public static class ChecksumGenerator
+{
+    public static byte Generate(byte[] data, int start, int length)
+    {
+        /*
+         *      Initialize the checksum to 255
+         *      For every byte from 0x2598 to 0x3522, inclusive, subtract its value from the checksum
+         *      $3522 is NOT a part of the checksum the wiki lies >:C
+         */
+        byte checksum = 0;
+
+        for (int i = 0; i < length - 1; i++)
+        {
+            checksum += data[start + i];
+        }
+
+        checksum = (byte)~checksum;
+
+        return checksum;
+    }
+}
