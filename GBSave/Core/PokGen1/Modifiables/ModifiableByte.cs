7db2874b316e8d6b540522946c6a841bfc718a0d
Snivo <jrgerba@gmail.com>
2022-04-10 16:49:33 -0400

    New Modifiable type

---
+++ GBSave/Core/PokGen1/Modifiables/ModifiableByte.cs
@@ -0,0 +1,12 @@
+﻿namespace GBSave.Core.PokGen1.Modifiables;
+
+class ModifiableByte : Modifiable
+{ 
+    public override object Read() => data[offset];
+
+    public override void Write(object obj) => data[offset] = (byte)obj;
+
+    public ModifiableByte(int offset, int length, Save save) : base(offset, length, save.Data)
+    {
+    }
+}
--- GBSave/Core/PokGen1/Modifiables/ModifiableString.cs
+++ GBSave/Core/PokGen1/Modifiables/ModifiableString.cs
@@ -12,12 +12,23 @@ public class ModifiableString : Modifiable
         for (int i = 0; i < length; i++)
             buffer[i] = data[offset + i];
 
-
+        return Gen1Encoding.Decode(buffer);
     }
 
     public override void Write(object val)
     {
-        string value = (string)val;
+        string? str = val as string;
+
+        byte[] encStr = Gen1Encoding.Encode(str);
+
+        int i = 0;
+        int siz = Math.Min(length - 1, encStr.Length);
+        for (; i < siz; i++)
+            data[offset + i] = encStr[i];
+        data[offset + i++] = 0x50;
+
+        for (; i < length; i++)
+            data[offset + i] = 0;
     }
 
     public ModifiableString(int offset, int length, Save save) : base(offset, length, save.Data)
