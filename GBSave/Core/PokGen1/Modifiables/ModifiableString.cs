9958b9feebe5c46471cf23ca13393505e2dfd103
Snivo <jrgerba@gmail.com>
2022-04-10 02:20:02 -0400

    Created "Modifiable" type boilerplate and gen 1 str type

---
+++ GBSave/Core/PokGen1/Modifiables/ModifiableString.cs
@@ -0,0 +1,28 @@
+﻿using GBSave;
+using GBSave.Core.PokGen1;
+
+namespace GBSave.Core.PokGen1.Modifiables;
+
+public class ModifiableString : Modifiable
+{
+    byte[] buffer;
+
+    public override string Read()
+    {
+        for (int i = 0; i < length; i++)
+            buffer[i] = data[offset + i];
+
+
+    }
+
+    public override void Write(object val)
+    {
+        string value = (string)val;
+    }
+
+    public ModifiableString(int offset, int length, Save save) : base(offset, length, save.Data)
+    {
+        buffer = new byte[length + 1];
+    }
+}
+
---
+++ GBSave/Modifiable.cs
@@ -0,0 +1,19 @@
+﻿namespace GBSave;
+
+public abstract class Modifiable
+{
+    protected int offset;
+    protected int length;
+
+    protected byte[] data;
+
+    public abstract void Write(object obj);
+    public abstract object Read();
+
+    public Modifiable(int offset, int length, byte[] data)
+    {
+        this.offset = offset;
+        this.length = length;
+        this.data = data;
+    }
+}
