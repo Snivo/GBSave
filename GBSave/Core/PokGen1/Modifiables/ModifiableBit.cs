9c3036aac3cc9b5ccabe9e9ea91750b9a81babc7
Snivo <jrgerba@gmail.com>
2022-04-11 22:17:58 -0400

    New Modifiable type

---
+++ GBSave/Core/PokGen1/Modifiables/ModifiableBit.cs
@@ -0,0 +1,20 @@
+﻿namespace GBSave.Core.PokGen1.Modifiables;
+
+public class ModifiableBit : Modifiable
+{
+    private readonly int bit;
+
+    public override object Read() => (data[offset] & (1 << bit)) != 0;
+
+    public override void Write(object obj)
+    {
+        bool set = (bool)obj;
+        
+        data[offset] = (byte)(set ? (data[offset] | (1 << bit)) : (data[offset] & ~(1 << bit)));
+    }
+
+    public ModifiableBit(int offset, int bit, Save save) : base(offset, 1, save.Data)
+    {
+        this.bit = bit;
+    }
+}
