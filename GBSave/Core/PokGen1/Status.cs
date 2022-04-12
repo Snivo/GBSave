d76ddb42fe0a1fb48f6270ea5a9b606d4260d916
Snivo <jrgerba@gmail.com>
2022-04-11 22:18:13 -0400

    Enumeration for status effects

---
+++ GBSave/Core/PokGen1/Status.cs
@@ -0,0 +1,11 @@
+﻿namespace GBSave.Core.PokGen1;
+
+public enum StatusCondition
+{
+    None        = 0x00,
+    Sleep       = 0x04,
+    Poisoned    = 0x08,
+    Burned      = 0x10,
+    Frozen      = 0x20,
+    Paralyzed   = 0x40,
+}
