2471c3e7fb93a39c2a4f943a03849b7a6cb7221b
Snivo <jrgerba@gmail.com>
2022-04-13 17:44:02 -0400

    Added pokemon data structure

---
+++ GBSave/Core/PokGen1/Pokemon.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GBSave.Core.PokGen1;
+
+public struct Pokemon
+{
+    public string Name { get; init; }
+    public int DexNum { get; init; }
+    public int BaseHP { get; init; }
+    public int BaseAttack { get; init; }
+    public int BaseDefense { get; init; }
+    public int BaseSpeed { get; init; }
+    public int BaseSpecial { get; init; }
+    public int Index { get; init; }
+
+    /* 
+     * Static Data 
+     */
+
+    public static readonly Pokemon[] PokemonList = new Pokemon[152];
+
+    static Pokemon()
+    {
+        PokemonList = JsonConvert.DeserializeObject<List<Pokemon>>(File.ReadAllText(@"Core\PokGen1\pkmndata.json")).ToArray();
+    }
+}
